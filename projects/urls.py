8d8f6592f070cc3f4f01a87233c6eefb193961b5
athira <athiratp2017@gmail.com>
2023-08-08 17:12:57 +0530

    new app created users and some files edited in third branch

--- projects/urls.py
+++ projects/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('projects/', views.projects, name='projects'),
-    path('project/<str:pk>/', views.project, name='project'),
+    path('', views.projects, name="projects"),
+    path('project/<str:pk>/', views.project, name="project"),
     path('create-project/', views.create_project, name="create-project"),
     path('update-project/<str:pk>/', views.update_project, name="update-project"),
     path('delete_project/<str:pk>/', views.deleteproject, name="delete-project"),
